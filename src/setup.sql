51df49311fcf540e1cff778ce554a1980c3f244e
josuemendozap <josue.mendozap@gmail.com>
2026-05-16 16:58:34 -0600

    Database Retrieval w02

--- src/setup.sql
+++ src/setup.sql
@@ -39,3 +39,55 @@ VALUES
 	(13, 'Elderly Support Visits', 'Weekly visits to assist elderly residents.', 'Puebla', '2026-08-03', 3),
 	(14, 'Clothing Donation Drive', 'Collecting and distributing clothing to those in need.', 'León', '2026-09-22', 3),
 	(15, 'Disaster Relief Support', 'Providing aid and supplies after natural disasters.', 'Veracruz', '2026-10-30', 3);
+
+CREATE TABLE category (
+	category_id SERIAL PRIMARY KEY,
+	name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE project_categories (
+	project_id INT,
+	category_id INT,
+	PRIMARY KEY (project_id, category_id),
+	FOREIGN KEY (project_id) REFERENCES service_project(project_id) ON DELETE CASCADE,
+	FOREIGN KEY (category_id) REFERENCES category(category_id) ON DELETE CASCADE
+);
+
+INSERT INTO category (name)
+VALUES
+	('Education'),
+	('Environment'),
+	('Health'),
+	('Community Service'),
+	('Sustainability');
+
+INSERT INTO project_categories (project_id, category_id)
+VALUES
+(1, 5),
+(1, 2),
+(2, 5),
+(2, 4),
+(3, 1),
+(3, 4),
+(4, 3),
+(4, 4),
+(5, 5),
+(5, 2),
+(6, 2),
+(6, 1),
+(7, 2),
+(7, 5),
+(8, 1),
+(8, 2),
+(9, 4),
+(9, 2),
+(10, 5),
+(10, 2),
+(11, 4),
+(11, 3),
+(12, 4),
+(12, 2),
+(13, 3),
+(14, 4),
+(15, 3),
+(15, 4);
