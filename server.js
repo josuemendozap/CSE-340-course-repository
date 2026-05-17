7733f3bd7f1bac6be53f981311398baef6b8b79d
josuemendozap <josue.mendozap@gmail.com>
2026-05-16 18:42:58 -0600

    Ready

--- server.js
+++ server.js
@@ -22,13 +22,14 @@ const app = express();
   * Configure Express middleware
   */
 
-// Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
 // Set EJS as the templating engine
 app.set('view engine', 'ejs');
 
 // Tell Express where to find your templates
 app.set('views', path.join(__dirname, 'src/views'));
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 /**
  * Routes
--- src/models/db.js
+++ src/models/db.js
@@ -14,9 +14,7 @@ import { Pool } from 'pg';
  */
 const pool = new Pool({
     connectionString: process.env.DB_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    ssl: true
 });
 
 /**
