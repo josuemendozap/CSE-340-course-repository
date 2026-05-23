4bb52fbd879f4188bf0b2087d52d12d473718406
josuemendozap <josue.mendozap@gmail.com>
2026-05-23 13:10:00 -0600

    model-view-controller

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
-import { testConnection } from './src/models/db.js';
+import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
-import express from 'express';
-import { getAllOrganizations } from './src/models/organizations.js';
-import { getAllProjects } from './src/models/projects.js';
-import { getAllCategories } from './src/models/categories.js';
+import { testConnection } from './src/models/db.js';
+import router from './src/routes.js';
 
-
-// Define the the application environment
+// Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
 
 // Define the port number the server will listen on
@@ -22,20 +19,14 @@ const app = express();
   * Configure Express middleware
   */
 
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Set EJS as the templating engine
 app.set('view engine', 'ejs');
 
 // Tell Express where to find your templates
 app.set('views', path.join(__dirname, 'src/views'));
-
-// Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Middleware to make NODE_ENV available to all templates
-app.use((req, res, next) => {
-    res.locals.NODE_ENV = NODE_ENV;
-    next();
-});
 
 // Middleware to log all incoming requests
 app.use((req, res, next) => {
@@ -45,41 +36,14 @@ app.use((req, res, next) => {
     next(); // Pass control to the next middleware or route
 });
 
-/**
- * Routes
- */
-app.get('/', async (req, res) => {
-    const title = 'Home';
-    res.render('home', { title });
+// Middleware to make NODE_ENV available to all templates
+app.use((req, res, next) => {
+    res.locals.NODE_ENV = NODE_ENV;
+    next();
 });
 
-app.get('/organizations', async (req, res) => {
-    const organizations = await getAllOrganizations();
-
-    const title = 'Our Partner Organizations';
-    res.render('organizations', { title, organizations });
-});
-
-app.get('/projects', async (req, res) => {
-    const projects = await getAllProjects();
-    
-    const title = 'Service Projects';
-    res.render('projects', { title, projects });
-});
-
-app.get('/categories', async (req, res) => {
-    const categories = await getAllCategories();
-
-    const title = 'Service Project Categories';
-    res.render('categories', { title, categories });
-});
-
-// Test route for 500 errors
-app.get('/test-error', (req, res, next) => {
-    const err = new Error('This is a test error');
-    err.status = 500;
-    next(err);
-});
+// Use the imported router to handle routes
+app.use(router);
 
 // Catch-all route for 404 errors
 app.use((req, res, next) => {
---
+++ src/controllers/categories.js
@@ -0,0 +1,10 @@
+import { getAllCategories } from "../models/categories.js";
+
+const categoriesPage = async (req, res) => {
+    const categories = await getAllCategories();
+
+    const title = 'Service Project Categories';
+    res.render('categories', { title, categories });
+};
+
+export { categoriesPage };
